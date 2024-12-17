9dd0f48e85e8583862169cb708bf0b65d931f6c5
Hussam Barouqa <hussam.barouqa@apryse.com>
2024-12-17 16:12:57 +0300

    Updated Webserver and baseUrl

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,7 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:1234',
+    baseURL: 'http://localhost:1234',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -74,8 +74,8 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
-    url: 'http://127.0.0.1:1234',
+    command: 'npm run build && npm run start',
+    url: 'http://localhost:1234',
     reuseExistingServer: !process.env.CI,
   },
 });
